af1d78a0f791fd5468e77a3f7b8a75322a137cec
nara <parknara0213@gmail.com>
2024-04-26 10:20:16 +0900

    240426 property attributes and property descriptor

--- 16-property-attributes.js
+++ 16-property-attributes.js
@@ -6,4 +6,25 @@ const o = {};
 
 // 일부 내부 슬롯과 내부 메서드에 한하여 간접적 접근 수단 제공
 o.__proto__; // Object.prototype
-// 콘솔에 아무것도 뜨지 않음 
+// 콘솔에 아무것도 뜨지 않음
+
+// 16.2.1 property attribute을 간접 확인하는 방법
+// const person = {
+//   name: "Lee",
+// };
+
+// console.log(Object.getOwnPropertyDescriptor(person, "name"));
+// {value: 'Lee', writable: true, enumerable: true, configurable: true}
+
+// 16.2.2
+const person = {
+  name: "Lee",
+};
+
+person.age = 20;
+
+console.log(Object.getOwnPropertyDescriptors(person));
+// {
+//      age : {value: 20, writable: true, enumerable: true, configurable: true}
+//      name : {value: 'Lee', writable: true, enumerable: true, configurable: true
+// }
