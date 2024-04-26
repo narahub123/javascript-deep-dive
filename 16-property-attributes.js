5e5eb4850719140a688627cd3910de20bdf0f60d
nara <parknara0213@gmail.com>
2024-04-26 11:24:49 +0900

    240426 accessor property

--- 16-property-attributes.js
+++ 16-property-attributes.js
@@ -17,14 +17,50 @@ o.__proto__; // Object.prototype
 // {value: 'Lee', writable: true, enumerable: true, configurable: true}
 
 // 16.2.2
-const person = {
-  name: "Lee",
-};
+// const person = {
+//   name: "Lee",
+// };
 
-person.age = 20;
+// person.age = 20;
 
-console.log(Object.getOwnPropertyDescriptors(person));
+// console.log(Object.getOwnPropertyDescriptors(person));
 // {
 //      age : {value: 20, writable: true, enumerable: true, configurable: true}
 //      name : {value: 'Lee', writable: true, enumerable: true, configurable: true
 // }
+
+// 16.3 accessor property
+const person = {
+  // data property
+  firstName: "Umgmo",
+  LastName: "Lee",
+
+  // fullName : accessor property
+  // getter
+  get fullName() {
+    return `${this.firstName} ${this.LastName}`;
+  },
+
+  // setter
+  set fullName(name) {
+    [this.firstName, this.LastName] = name.split(" ");
+  },
+};
+
+// 데이터 프로퍼티를 통한 프로퍼티 값의 참조 
+console.log(person.firstName + " " + person.LastName); // Umgmo Lee
+
+// 접근자 프로퍼티를 통한 프로퍼티 값의 저장 
+person.fullName = "Heegun Lee";
+console.log(person); // {firstName: 'Heegun', LastName: 'Lee'}
+
+// 접근자 프로퍼티를 통한 프로퍼티 값의 참조 
+console.log(person.fullName); // Heegun Lee
+
+// firstName은 데이터 프로퍼티 
+let descriptor = Object.getOwnPropertyDescriptor(person, "firstName");
+console.log(descriptor); // {value: 'Heegun', writable: true, enumerable: true, configurable: true}
+
+// fullName은 접근자 프로퍼티 
+descriptor = Object.getOwnPropertyDescriptor(person, "fullName");
+console.log(descriptor); // {enumerable: true, configurable: true, get: ƒ, set: ƒ}
